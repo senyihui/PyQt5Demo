ea861083937a0b3654ac88e4ddc7d95476c8e53a
Syh <986245600@qq.com>
2020-10-26 18:50:30 +0800

    update

--- NewInterface.py
+++ NewInterface.py
@@ -55,4 +55,4 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Title"))
